88bafd50c6c0e29061b1e3b42d8006317c07f113
bkkimutai <kbmsama@gmail.com>
2023-09-24 23:44:51 +0300

    Squad class - setters and setters

--- src/main/java/org/bkkimutai/Squad.java
+++ src/main/java/org/bkkimutai/Squad.java
@@ -1,4 +1,62 @@
 package org.bkkimutai;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Squad {
+    private String mSquadName;
+    private  String mDescription;
+    private static List<Squad> instances = new ArrayList<Squad>();
+    private int mId;
+    private List<Hero> mHeroes;
+
+
+    public Squad (String squadName, String description){
+        mSquadName = squadName;
+        mDescription= description;
+        instances.add(this);
+        mId = instances.size();
+        mHeroes = new ArrayList<Hero>();
+
+    }
+
+    public String getmSquadName() {
+        return mSquadName;
+    }
+
+    public void setmSquadName(String mSquadName) {
+        this.mSquadName = mSquadName;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public void setmDescription(String mDescription) {
+        this.mDescription = mDescription;
+    }
+
+    public static List<Squad> getInstances() {
+        return instances;
+    }
+
+    public static void setInstances(List<Squad> instances) {
+        Squad.instances = instances;
+    }
+
+    public int getmId() {
+        return mId;
+    }
+
+    public void setmId(int mId) {
+        this.mId = mId;
+    }
+
+    public List<Hero> getmHeroes() {
+        return mHeroes;
+    }
+
+    public void setmHeroes(List<Hero> mHeroes) {
+        this.mHeroes = mHeroes;
+    }
 }
